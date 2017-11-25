65556436daa2338ed18b0c11d15c0704885dda88
xinye.li <xinye.li@try4.org>
2017-11-25 23:55:09 +0800

    web path

--- src/config/server.js
+++ src/config/server.js
@@ -3,5 +3,5 @@ import path from 'path'
 export const server = {
   httpPort: 80,
   httpsPort: 443,
-  servePath: path.resolve('../../com.iget365.web/dist')
+  servePath: path.resolve('../com.iget365.web/dist')
 }
